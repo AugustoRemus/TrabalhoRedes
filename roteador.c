d6aab1fddf3b1836af0d70ed359c66ec9a6edfc1
Augusto Renostro Remus <131462760+AugustoRemus@users.noreply.github.com>
2025-08-29 17:50:56 -0300

    Inicio struct

---
+++ roteador.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <string.h>
+
+#define Controle 0;
+#define Dado 1;
+
+typedef struct{
+    int tipo;//tipo de msg
+    int origem;//quem enviou
+    int destino;//qual é o destino
+    // para fazer rota calcular no inicio
+    char conteudo[500]; //texto que a msg vai mandar
+} Mensagem;
+
+//Mensagem minhaMensagem;
+
+//terminal é a main
+int main(){
+
+
+    
+}
+
